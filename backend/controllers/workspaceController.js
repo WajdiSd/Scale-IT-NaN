ee7814902c6654e9039b5dbdcc4e3d7b3504f110
omar karboul <karboulomar@gmail.com>
2022-03-16 18:12:27 +0100

    add workspace / fixed schema

--- backend/controllers/workspaceController.js
+++ backend/controllers/workspaceController.js
@@ -1,84 +1,84 @@
-const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const Workspace = require("../models/workspaceModel");
+const Member = require("../models/memberModel");
 
-//generate JWT
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+// @desc Get goals
+// @route get /api/workspace
+// @access private
+const getWorkspaces = asyncHandler(async (req, res) => {
+  const workspaces = await Workspace.find({
+    "assigned_members.member": req.params.id,
   });
-};
+  res.status(200).json(workspaces);
+});
 
 // @desc add new workspace
 // @route post /api/workspace
 // @access public
 const addWorkspace = asyncHandler(async (req, res) => {
-  const {
-    name,
-    description,
-    archive,
-  } = req.body;
-  if (
-    !name ||
-    !description ||
-    !archive 
-  ) {
+  const memb = await Member.findById(req.params.id);
+  const { name, description } = req.body;
+  if (!name || !description) {
     res.status(400);
     throw new Error("please add all fields");
   }
 
-  //check if member exists with email
+  //check if workspace exists with name
   const workspaceExist = await Workspace.findOne({ name });
 
   if (workspaceExist) {
     res.status(400);
     throw new Error("workspace already exists");
   }
+
+  const obj = {
+    member: req.params.id,
+    isHR: true,
+  };
   //create workspace
   const wkspc = await Workspace.create({
     name,
     description,
-    archive
+    $push: { "assigned_members.member_workspace": obj },
   });
+  console.log(wkspc);
 
   if (wkspc) {
-    res.status(201).json({
-      _id: wkspc._id,
-      name: wkspc.name,
-      description: wkspc.description,
-      token: generateToken(wkspc._id),
-    });
-  } 
-  else {
+    res.status(201).json(wkspc);
+  } else {
     res.status(400);
     throw new Error("invalid workspace data");
   }
 });
 
-
 // Update workspace
-// @desc update workspace 
+// @desc update workspace
 // @route post /api/workspace/update/:id
 // @access public
 const updateWksp = asyncHandler(async (req, res) => {
   //
-const entries = Object.keys(req.body)
-const updates = {}
+  const entries = Object.keys(req.body);
+  const updates = {};
 
-// constructing dynamic query : get the informations entered in BODY
-for (let i = 0; i < entries.length; i++) {
-  updates[entries[i]] = Object.values(req.body)[i]
+  // constructing dynamic query : get the informations entered in BODY
+  for (let i = 0; i < entries.length; i++) {
+    updates[entries[i]] = Object.values(req.body)[i];
   }
-// update members fields according to the BODY
-Workspace.updateOne({_id: req.params.id} , {$set: updates} ,
-function (err , success) {
-  if (err) throw (err);
-    else {
-    res.send({msg: "update success"})
-    ;}})
-});  
+  // update members fields according to the BODY
+  Workspace.updateOne(
+    { _id: req.params.id },
+    { $set: updates },
+    function (err, success) {
+      if (err) throw err;
+      else {
+        res.send({ msg: "update success" });
+      }
+    }
+  );
+});
 
 module.exports = {
   addWorkspace,
-  updateWksp
+  updateWksp,
+  getWorkspaces,
 };
--- backend/routes/workspaceRoutes.js
+++ backend/routes/workspaceRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   addWorkspace,
-  updateWksp
+  updateWksp,
+  getWorkspaces,
 } = require("../controllers/workspaceController");
 
 const { protect } = require("../middleware/authMiddleware");
 
-router.post("/add", addWorkspace);
+router.post("/add/:idmember", addWorkspace);
 router.put("/update/:id", updateWksp);
-
+router.get("/:id", getWorkspaces);
 
 module.exports = router;
