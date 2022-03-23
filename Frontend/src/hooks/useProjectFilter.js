27bf2ddb94115746db21985c630d89120db9f5cf
Bilel Taktak <bilel.taktak@esprit.tn>
2022-03-23 01:20:44 +0100

    refactoring: removed one line from useProjectFilter

--- Frontend/src/hooks/useProjectFilter.js
+++ Frontend/src/hooks/useProjectFilter.js
@@ -9,14 +9,11 @@ const useProjectFilter = (projects) => {
 
   useEffect(() => (query ? searchProjectsFilter(query) : setProjectsFilter(initialProjects)), [initialProjects, query]);
 
-  const searchProjectsFilter = (value) =>
+  const searchProjects = (value) => {
+    setQuery(value);
     setProjectsFilter(() =>
       initialProjects.filter((project) => project.name.toUpperCase().includes(value.toUpperCase()))
     );
-
-  const searchProjects = (value) => {
-    setQuery(value);
-    searchProjectsFilter(query);
   };
 
   return { query, projectsFilter, searchProjects };
