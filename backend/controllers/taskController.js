d9aa05c81a1e15e8753edc85572c3cd1c99f13ed
omar karboul <karboulomar@gmail.com>
2022-04-02 08:44:16 +0100

    add task back

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,55 @@
 const asyncHandler = require("express-async-handler");
 const Project = require("../models/projectModel");
 const Member = require("../models/memberModel");
-const Workspace = require("../models/workspaceModel");
+const Task = require("../models/taskModel");
 
-module.exports = {};
+const addTask = asyncHandler(async (req, res) => {
+  const {
+    name,
+    description,
+    startDate,
+    expectedEndDate,
+    teamLeadId,
+    projectId,
+  } = req.body;
+  if (!name || !description || !startDate || !expectedEndDate) {
+    res.status(400);
+    throw new Error("please add all fields");
+  }
+
+  const project = await Project.findById(projectId);
+  if (!project) {
+    res.status(404);
+    throw new Error("project not found");
+  }
+  project.assigned_members.forEach((element) => {
+    if (element.memberId.equals(teamLeadId)) {
+      if (!element.isTeamLeader) {
+        res.status(403);
+        throw new Error("you are not allowed to create a task");
+      }
+    }
+  });
+
+  const member = await Member.findById(teamLeadId);
+  if (!member) {
+    res.status(404);
+    throw new Error("invalid team leader id");
+  }
+
+  const task = await Task.create({
+    name,
+    description,
+    startDate,
+    expectedEndDate,
+    project: projectId,
+  }).catch((err) => {
+    res.status(400);
+    throw new Error("could not create task", err);
+  });
+  res.status(201).json({ task });
+});
+
+module.exports = {
+  addTask,
+};
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { addTask } = require("../controllers/taskController");
 
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
+
+router.post("/add", protect, addTask);
 
 module.exports = router;
