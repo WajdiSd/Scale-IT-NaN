7e75b631dd49bfa4d3e2e2d06e75434491d731bc
omar karboul <karboulomar@gmail.com>
2022-04-22 01:14:07 +0100

    fix conflicts

--- backend/routes/performanceRoutes.js
+++ backend/routes/performanceRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const {
-  getPerformanceByMember,
   getleaderboardbyworkspace,
   getleaderboardbyproject,
   test,
-<<<<<<< HEAD
   getMemberTasksContribution,
   getRankProjectLeaderboard,
   getRankWorkspaceLeaderboard,
-=======
   getLateTasksPercentage,
->>>>>>> 4d039d953bc7618da1f8ca8990c74e534795be99
 } = require("../controllers/performanceController");
 const router = express.Router();
 
@@ -18,7 +14,7 @@ const router = express.Router();
 router.get("/leaderboard-workspace/:workspaceid", getleaderboardbyworkspace);
 router.get("/leaderboard-project/:projectid", getleaderboardbyproject);
 router.get("/test", test);
-<<<<<<< HEAD
+
 router.get(
   "/getcontribbution/:projectId/:memberId",
   getMemberTasksContribution
@@ -33,8 +29,7 @@ router.get(
   "/getrankworkspaceleaderboard/:workspaceid/:memberId",
   getRankWorkspaceLeaderboard
 );
-=======
+
 router.get("/latetasks/:idproj/:idmember", getLateTasksPercentage);
->>>>>>> 4d039d953bc7618da1f8ca8990c74e534795be99
 
 module.exports = router;
