8afaf23de3b4131496217d201552aba7888d87f4
WajdiSd <wajdi.sadouki@esprit.tn>
2022-05-05 20:43:26 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "public")));
 
