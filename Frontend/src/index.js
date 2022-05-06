c4c92ddc54351dbade92a3a47e04889da2bfee78
omar karboul <karboulomar@gmail.com>
2022-05-06 12:07:30 +0100

    dep

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,26 +4,26 @@ import './locales/i18n';
 // highlight
 import './utils/highlight';
 
-// // scroll bar
-// import 'simplebar/src/simplebar.css';
+// scroll bar
+import 'simplebar/src/simplebar.css';
 
-// // lightbox
-// import 'react-image-lightbox/style.css';
+// lightbox
+import 'react-image-lightbox/style.css';
 
-// // map
-// import 'mapbox-gl/dist/mapbox-gl.css';
+// map
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-// // editor
-// import 'react-quill/dist/quill.snow.css';
+// editor
+import 'react-quill/dist/quill.snow.css';
 
-// // slick-carousel
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+// slick-carousel
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-// // lazy image
-// import 'react-lazy-load-image-component/src/effects/blur.css';
-// import 'react-lazy-load-image-component/src/effects/opacity.css';
-// import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+// lazy image
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
