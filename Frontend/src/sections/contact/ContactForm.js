70b588316b4cc1fba322cf9dcf5c6d91c4118b8c
WajdiSd <wajdi.sadouki@esprit.tn>
2022-05-29 17:05:06 +0100

    finished contact form

--- Frontend/src/sections/contact/ContactForm.js
+++ Frontend/src/sections/contact/ContactForm.js
@@ -1,11 +1,67 @@
 // @mui
 import { Button, Typography, TextField, Stack } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 // components
 import { MotionInView, varFade } from '../../components/animate';
+import { FormProvider, RHFTextField } from 'src/components/hook-form';
+import { useDispatch } from 'react-redux';
+import { sendContactEmail } from 'src/redux/slices/authSlice';
 
 // ----------------------------------------------------------------------
 
 export default function ContactForm() {
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const NewWorkspaceSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    message: Yup.string().required('Message is required'),
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    subject: Yup.string().required('Subject is required'),
+
+  });
+
+  const defaultValues = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewWorkspaceSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting, isValid },
+  } = methods;
+
+  const values = watch();
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data) => {
+    try {
+      dispatch(sendContactEmail(data))
+      .then(res=>{
+        enqueueSnackbar("Email sent successfully");
+        reset();
+      });
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Stack spacing={5}>
       <MotionInView variants={varFade().inUp}>
@@ -14,30 +70,31 @@ export default function ContactForm() {
           We'll be glad to hear from you, buddy.
         </Typography>
       </MotionInView>
-
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={3}>
         <MotionInView variants={varFade().inUp}>
-          <TextField fullWidth label="Name" />
+          <RHFTextField fullWidth label="Name" name="name" />
         </MotionInView>
 
         <MotionInView variants={varFade().inUp}>
-          <TextField fullWidth label="Email" />
+          <RHFTextField fullWidth label="Email" name="email" />
         </MotionInView>
 
         <MotionInView variants={varFade().inUp}>
-          <TextField fullWidth label="Subject" />
+          <RHFTextField fullWidth label="Subject" name="subject" />
         </MotionInView>
 
         <MotionInView variants={varFade().inUp}>
-          <TextField fullWidth label="Enter your message here." multiline rows={4} />
+          <RHFTextField fullWidth label="Enter your message here." name="message" multiline rows={4} />
         </MotionInView>
       </Stack>
 
       <MotionInView variants={varFade().inUp}>
-        <Button size="large" variant="contained">
+        <Button type="submit" sx={{mt: 3}} size="large" variant="contained">
           Submit Now
         </Button>
       </MotionInView>
+      </FormProvider>
     </Stack>
   );
 }
